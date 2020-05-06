0352dd24bdc3a3fb6cbf3b25df0fd5e89b4bb09a
Bulova <bulova.m.i@gmail.com>
2020-05-06 22:46:47 +0300

    add task6 - initial conditions

--- src/main/kotlin/subtask6/FullBinaryTrees.kt
+++ src/main/kotlin/subtask6/FullBinaryTrees.kt
@@ -2,8 +2,15 @@ package subtask6
 
 class FullBinaryTrees {
 
-    // TODO: Complete the following function
     fun stringForNodeCount(count: Int): String {
+        if(count == 1) return "[[0]]"
+        if(count % 2 == 0) return "[]"
+        if(count == 3) return "[0,0,0]"
+
+        return generateAllTrees(count)
+    }
+
+    private fun generateAllTrees(count: Int): String{
         throw NotImplementedError("Not implemented")
     }
 }
