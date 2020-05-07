ce3a0c27d12661832a523807924bb1ead78b8155
Bulova <bulova.m.i@gmail.com>
2020-05-07 14:44:36 +0300

    Add task6

--- src/main/kotlin/subtask6/FullBinaryTrees.kt
+++ src/main/kotlin/subtask6/FullBinaryTrees.kt
@@ -7,10 +7,27 @@ class FullBinaryTrees {
         if(count % 2 == 0) return "[]"
         if(count == 3) return "[0,0,0]"
 
-        return generateAllTrees(count)
+        generateTrees("0,0,0", 0, count - 3)
+
+        val resultString = listOfTrees.joinToString(separator = ",")
+        return "[$resultString]"
     }
 
-    private fun generateAllTrees(count: Int): String{
-        throw NotImplementedError("Not implemented")
+    private fun generateTrees(tree: String, currentRootIndex: Int, rest: Int){
+        if(rest == 0) {
+            listOfTrees.add("[$tree]")
+        } else{
+            if(tree[currentRootIndex] == '0') {
+                generateTrees("$tree,0,0", currentRootIndex + 1, rest - 2)
+                generateTrees("$tree,null,null", currentRootIndex + 1, rest)
+            } else {
+                val next = tree.indexOf('0', currentRootIndex+1)
+                if(next!= -1) generateTrees(tree, next, rest)
+            }
+        }
+    }
+
+    private companion object{
+        var listOfTrees = ArrayList<String>()
     }
 }
